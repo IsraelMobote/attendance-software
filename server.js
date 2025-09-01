4bfba582f72bc562ae42289af486a130a22ee15b
IsraelMobote <moboteisrael02@gmail.com>
2025-09-01 18:42:48 +0100

    edit

--- server.js
+++ server.js
@@ -1,15 +1,71 @@
-/**
- * This is the application server
- */
 
+/* ******************************************
+ * This server.js file is the primary file of the 
+ * application. It is used to control the project.
+ *******************************************/
+
+/* ***********************
+ * Require Statements
+ *************************/
 const express = require("express")
 const app = express()
+const env = require("dotenv").config()
+const expressLayouts = require("express-ejs-layouts")
+const bodyParser = require("body-parser")
+const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res)
+    next()
+})
+
+
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+/* ***********************
+ * Routes
+ *************************/
+app.use(static)
+
+// Index route
+app.get("/", baseController.buildHome)
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+    next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+    console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+    if (err.status == 404) {
+        message = err.message
+    }
+    else { message = 'Oh no! There was a crash. Maybe try a different route?' }
+    res.render("errors/error", {
+        title: err.status || 'Server Error',
+        message
+    })
+})
+
 
 /**
  * server host name and port
  */
 const HOST = 'localhost'
-const PORT = 3000
+const PORT = 5500
 
 /**
  * Log statement to confirm server operation
@@ -21,4 +77,4 @@ app.listen(PORT, () => {
 /**
  * Default GET route
  */
-app.get("/", (req,res) => {res.send("Welcome home!")})
+app.get("/", (req, res) => { res.send("Welcome home!") })
